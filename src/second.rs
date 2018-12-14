f1696d61d2e89331c9975968c7ba0c2586d4d37e
sepiropht <sergembotta@mailoo.org>
2018-12-14 23:44:05 +0100

    chap 3.5

--- src/second.rs
+++ src/second.rs
@@ -1,6 +1,6 @@
 
 pub struct List<T> {
-    head: Link<T> ,
+    head: Link<T>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -30,6 +30,31 @@ impl<T> List<T> {
             self.head = node.next;
             node.elem
         })
+    }
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
+}
+
+pub struct IntoIter<T> (List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -66,4 +91,28 @@ mod test {
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
     }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+
+        list.push("william");
+        assert_eq!(list.peek(), Some(&"william"));
+        list.push("annick");
+        assert_eq!(list.peek(), Some(&"annick"));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+
+        assert_eq!(Some(3), iter.next());
+        assert_eq!(Some(2), iter.next());
+        assert_eq!(Some(1), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
 }
