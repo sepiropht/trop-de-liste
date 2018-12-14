3b5bc429f54b1e3d0ce37dddf4f7694f80504381
sepiropht <sergembotta@mailoo.org>
2018-12-14 13:51:47 +0100

    ch 2.6

--- src/first.rs
+++ src/first.rs
@@ -10,7 +10,7 @@ enum Link {
 }
 
 struct Node {
-    elem: i32,
+    elem: String,
     next: Link,
 }
 
@@ -18,12 +18,21 @@ impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
     }
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: String) {
         let new_node = Box::new(Node {
             elem,
             next: mem::replace(&mut self.head, Link::Empty)
         });
 
         self.head = Link::More(new_node);
+    }
+    pub fn pop(&mut self) -> Option<String> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        } 
     }
 }
